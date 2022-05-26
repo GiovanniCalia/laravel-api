4b677cdaa88e91acee64709aa11b6981527b314c
GiovanniCalia <johnca@outlook.it>
2022-05-26 18:39:11 +0200

    Adjustments about the pagination

--- resources/js/components/HeaderPage.vue
+++ resources/js/components/HeaderPage.vue
@@ -1,5 +1,7 @@
 <template>
-    <header>Header</header>
+    <header>
+        <h1 class="text-center mt-3">Header</h1>
+        </header>
 </template>
 
 <script>
--- resources/js/components/MainPage.vue
+++ resources/js/components/MainPage.vue
@@ -1,22 +1,85 @@
 <template>
+<div>
     <div class="row">
-        <div class="card g-3" style="width: 18rem;" v-for="i in 5">
+        <li class="text-center">
+            <h5>Page {{ currentPage }} of {{ lastPage }}</h5>
+        </li>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination justify-content-center">
+                <li class="page-item" :class="{disabled : currentPage === 1}"  @click='getArray(prevPageUrl)'>
+                    <a class="page-link" :href="prevPageUrl">Previous</a>
+                </li>
+
+                <li class="page-item mx-2 d-flex align-items-center">
+                    <form @submit.prevent="getArray(apiUrl + '/?page=' + nNewPage)">
+                        <input type="text" v-model="nNewPage" placeholder="Insert page">
+                    </form>
+                </li>
+
+                <li class="page-item" :class="{disabled : currentPage === LastPage}" @click='getArray(nextPageUrl)'>
+                    <a class="page-link" :href="nextPageUrl">Next</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+      <div class="row">
+        <div class="card m-3" style="width: 18rem;" v-for="post in posts" :key="post.id">
             <img src="" class="card-img-top" alt="">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="card-body d-flex flex-column">
+                <h5 class="card-title">{{ post.title }}</h5>
+                <p class="card-text">{{ post.description }}</p>
+                <a href="#" class="btn btn-primary mt-auto">View</a>
             </div>
         </div>
     </div>
+</div>
 </template>
 
 <script>
 export default {
-    name: 'MainPage'
+    name: 'MainPage',
+    data () {
+        return {
+            posts: [],
+
+            newPage: null,
+
+            apiUrl: 'http://127.0.0.1:8000/api/posts',
+
+            currentPage: null,
+            firstPageUrl: null,
+            lastPage: null,
+            lastPageUrl: null,
+            nextPageUrl: null,
+            prevPageUrl: null,
+        }
+    },
+    created() {
+        this.getArray('http://127.0.0.1:8000/api/posts')
+    },
+    methods: {
+        getArray(http) {
+            if (http) {
+                Axios.get(http)
+                .then(element => {
+                    this.posts = element.data.response.data;
+                    this.currentPage = element.data.response.current_page;
+                    this.firstPageUrl = element.data.response.first_page_url;
+                    this.lastPage = element.data.response.last_page;
+                    this.lastPageUrl = element.data.response.last_page_url;
+                    this.nextPageUrl = element.data.response.next_page_url;
+                    this.prevPageUrl = element.data.response.prev_page_url;
+                })
+            }
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+li{
+    list-style-type: none;
+}
 
 </style>
